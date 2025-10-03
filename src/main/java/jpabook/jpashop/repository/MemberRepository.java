7e6fdd61ba78bfbf065b2f31bbadcb72e807d5b1
subinpar <bagsubin565@gmail.com>
2025-10-03 19:36:04 +0900

    회원 도메인 완료

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -24,12 +24,12 @@ public class MemberRepository {
     }
 
     public List<Member> findAll(){
-        return em.createQuery("select m form Member m", Member.class)
+        return em.createQuery("select m from Member m", Member.class)
                 .getResultList();
     }
 
     public List<Member> findByName(String name){
-        return em.createQuery("select m form Member m.name=:name", Member.class)
+        return em.createQuery("select m from Member m where m.name=:name", Member.class)
                 .setParameter("name",name)
                 .getResultList();
     }
